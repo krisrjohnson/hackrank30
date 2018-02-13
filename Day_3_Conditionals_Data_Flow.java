57ea1f4e23a9e86c5edd061eb0e2015afd787445
unknown <kris.jhnsn@gmail.com>
2018-02-13 15:37:05 -0500

    Day 3, if else conditions

---
+++ Day_3_Conditionals_Data_Flow.java
@@ -0,0 +1,35 @@
+// Day 3
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+   
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt(); 
+      scan.close();
+      String ans="";
+          
+      // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+      if(n%2==1){
+         ans = "Weird";
+      }
+      else if (n < 5 && n >= 2) {
+          ans = "Not Weird";
+      } else if (n < 20 && n >= 6) {
+          ans = "Weird";
+      } else if (n >= 20) {
+          ans = "Not Weird";
+      }
+      System.out.println(ans);
+
+/*   If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+*/   
+   
+   }
+}
