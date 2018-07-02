725a3064cd3bb138ac401725416bcbe66542269d
Kris <kris.jhnsn@gmail.com>
2018-07-02 18:31:07 -0400

    combining tax and tip apply into single line, adding comments

--- Day_2_data_types.java
+++ Day_2_data_types.java
@@ -15,12 +15,13 @@ public class Solution {
         int tip_percent = in.nextInt();
         int tax_percent = in.nextInt();
         in.close();
-        double tip;
-        double tax;
+        double tax_tip;
         int meal_total;
         
-        tip = tip_percent * meal_cost * .01;
-        tax = tax_percent * meal_cost * .01;
+        // int times double convets to double, the .01 is bc tip_percent and tax_percent are percentages
+        tax_tip = (tip_percent + tax_percent) * meal_cost * .01;
+                
+        // output requested to be rounded to nearest int:
         meal_total = (int) Math.round(meal_cost + tip + tax);
         
         //System.out.println(meal_total);
